447584f37bb892fc7a3e01268256717cf2dd1a58
Josh Hertz <jhertz1@gmail.com>
2020-09-16 17:02:39 -0700

    Successfully got user's location automatically

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,8 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Constants } from "expo";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 import MapView from "react-native-maps";
 import * as firebase from "firebase";
 import "firebase/firestore";
@@ -10,13 +13,80 @@ import axios from "axios";
 import breezy_key from "./breezy.js";
 import Map from "./Map.js";
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button title="Go to Map" onPress={() => navigation.navigate("Map")} />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {},
+      errorMessage: null,
+    };
+  }
+  // componentWillMount() {
+  //   this.findCurrentLocationAsync();
+  // }
+
+  findCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = JSON.stringify(position.coords.latitude);
+        const longitude = JSON.stringify(position.coords.longitude);
+        this.setState({
+          latitude,
+          longitude,
+        });
+      },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }
+
+  findCurrentLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied",
+      });
+      console.log("inside findcurrentasync", this.state);
+    }
+
+    let location = await Location.getCurrentPositionAsync();
+    this.setState({ location });
+  };
+
+  render() {
+    console.log(this.state);
+    let text = "";
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <TouchableOpacity
+          onPress={() => {
+            this.findCurrentLocationAsync();
+          }}
+        >
+          <Text>Where am I?</Text>
+          <Text>{text}</Text>
+        </TouchableOpacity>
+        <Button
+          title="Go to Map"
+          onPress={() => this.props.navigation.navigate("Map")}
+        />
+      </View>
+    );
+  }
 }
+
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Home Screen</Text>
+//       <Button title="Go to Map" onPress={() => navigation.navigate("Map")} />
+//     </View>
+//   );
+// }
 
 export default HomeScreen;
